6ca515bdb9188cce52fd2ebb903516e41f065cd3
anaghadeshpandek@gmail.com <anaghadeshpandek@gmail.com>
2024-08-22 13:42:17 +0530

    p11 to p13 added

---
+++ python/p11.py
@@ -0,0 +1,10 @@
+#Problem statement : Program to find sum or digits of a number
+
+num=int(input('Enter the number whose sum of digits is to be found: '))
+t = num
+sum = 0
+while num != 0:
+    j = num % 10
+    sum = sum + j
+    num = num // 10
+print(sum, ' is the sum of digit in ', t) 
---
+++ python/p13.py
@@ -0,0 +1,12 @@
+#Program to find biggest and smallest digits in a number
+
+num = int(input('Enter the number whose biggest and smallest digits are to be found: '))
+t = num
+digit = []
+while num != 0:
+    i = num % 10
+    digit.append(i)
+    num = num // 10
+digit.sort()
+print(digit[0],' is the smallest digit in ', t)
+print(digit[-1],' is the biggest digit in ', t)
