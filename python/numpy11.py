1f941675750aff255ed079109fb203c66079c589
anaghadeshpandek@gmail.com <anaghadeshpandek@gmail.com>
2024-08-30 16:34:58 +0530

    add

---
+++ python/numpy11.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+arr1 = np.arange(12)
+
+arr2 = arr1.reshape(2, 6)
+arr3 = arr1.reshape(6, 2)
+arr4 = arr1.reshape(3, 4)
+arr5 = arr1.reshape(12, 1)
+
+print(arr1)
+print(arr2)
+print(arr3)
+print(arr4)
+print(arr5) 
